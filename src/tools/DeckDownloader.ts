7b1beab5b835d291b5ab249f21f9e6d61505ab43
Alexander Fritsch <35202909+loudar@users.noreply.github.com>
2025-08-12 12:16:34 +0200

    add archidekt import

--- src/tools/DeckDownloader.ts
+++ src/tools/DeckDownloader.ts
@@ -1,10 +1,19 @@
 import type {Card, CardLine, Deck} from "../models/MTG.ts";
 import {cardLine} from "./Converters.ts";
 import {ScryfallApi} from "./ScryfallApi.ts";
+import axios from "axios";
+import type {ArchidektDeck} from "./ArchidektDeck.ts";
 
 export class DeckDownloader {
     static async getFromDeckUrl(deckUrl: string): Promise<Deck> {
-        const cardLines: CardLine[] = [];
+        const urlObj = new URL(deckUrl);
+        let cardLines: CardLine[] = [];
+
+        switch (urlObj.origin) {
+            case "https://archidekt.com":
+                cardLines = await DeckDownloader.getCardLinesFromArchidekt(deckUrl);
+                break;
+        }
 
         return DeckDownloader.getFromCardList(cardLines);
     }
@@ -23,11 +32,11 @@ export class DeckDownloader {
                 const count = line.count ?? 1;
                 for (let i = 0; i < count; i++) {
                     card.isCommander = false;
-                    if (line.categories?.includes("Commander{top}")) {
+                    if (line.categories?.some(c => c.startsWith("Commander"))) {
                         card.isCommander = true;
                         deck.commanders ??= [];
                         deck.commanders.push(card);
-                    } else if (line.categories?.includes("Attraction{noDeck}")) {
+                    } else if (line.categories?.some(c => c.startsWith("Attraction"))) {
                         deck.attractions ??= [];
                         deck.attractions.push(card);
                     } else {
@@ -43,10 +52,26 @@ export class DeckDownloader {
     }
 
     static async getFromString(input: string): Promise<Deck> {
-        if (input.includes("http")) {
+        if (input.startsWith("http")) {
             return await DeckDownloader.getFromDeckUrl(input);
         }
 
         return await DeckDownloader.getFromCardList(input.split("\n").map(line => cardLine(line)));
+    }
+
+    private static async getCardLinesFromArchidekt(deckUrl: string) {
+        const deckId = deckUrl.split("/").at(-2);
+        if (!deckId) {
+            throw new Error("Invalid deck URL");
+        }
+
+        const url = `https://archidekt.com/api/decks/${deckId}/?format=json`;
+        const res = await axios.get<ArchidektDeck>(url);
+        const archideck = res.data;
+        return archideck.cards.map(c => <CardLine>{
+            name: c.card.oracleCard.name,
+            count: c.quantity,
+            categories: c.categories
+        });
     }
 }
